17a29b72e1d3088676f7681dd3bce5384f75beda
Peter Georgiev <peroxmen@hotmail.com>
2017-09-28 23:39:05 +0300

    Added OOPBasics/Lab/04. PHP-OOP-Basics-Lab.docx, OOPBasics/Lab/DefineClassCar.php, OOPBasics/Lab/DefineClassCarInput.php.

--- OOPBasics/Lab/DefineClassCar.php
+++ OOPBasics/Lab/DefineClassCar.php
@@ -1,16 +1,30 @@
 <?php
 declare(strict_types=1);
-/* Problem 01. Define a class Car */
+
+namespace SoftUni\Oop;
+/* Problem 01. Define a class Car
+*  Problem 02.	Add a Constructor
+*  Problem 03.	Add a methods
+*/
+
 class Car
 {
     private $brand;
-    private $model;
     private $year;
 
-    public function __construct(string $brand, string $model)
+    /**
+     * Car constructor.
+     * @param string $brand
+     * @param string $model
+     */
+    public function __construct(string $brand,
+                                string $model,
+                                string $engine,
+                                string $numberOfSeats,
+                                string $horsepower)
     {
         $this->brand = $brand;
-        $this->model = $model;
+        $this->model = new Model($model, $engine, $numberOfSeats, $horsepower);
     }
 
     /**
@@ -18,10 +32,11 @@ class Car
      */
     public function setYear(int $year)
     {
-        if ($year > 1940 && $year < 3000){
+        if ($year > 999 && $year < 3001) {
             $this->year = $year;
+        } else {
+            throw new \Exception("Error: year from 1000 to 3000.");
         }
-        $this->year = 0;
     }
 
     /**
@@ -38,6 +53,54 @@ class Car
     public function getYear()
     {
         return $this->year;
+    }
+
+    /**
+     * @return Model
+     */
+    public function getModel(): Model
+    {
+        return $this->model;
+    }
+}
+
+class Model
+{
+    private $model;
+    private $engine;
+    private $numberOfSeats;
+    private $horsepower;
+
+    public function __construct(string $model, string $engine, string $numberOfSeats, string $horsepower)
+    {
+        $this->model = $model;
+        $this->engine = $engine;
+        $this->numberOfSeats = $numberOfSeats;
+        $this->horsepower = $horsepower;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEngine(): string
+    {
+        return $this->engine;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNumberOfSeats(): string
+    {
+        return $this->numberOfSeats;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHorsepower(): string
+    {
+        return $this->horsepower;
     }
 
     /**
--- OOPBasics/Lab/DefineClassCarInput.php
+++ OOPBasics/Lab/DefineClassCarInput.php
@@ -2,24 +2,78 @@
 declare(strict_types=1);
 include "DefineClassCar.php";
 
-for ($i = 0; $i < 4; $i++) {
-    $arr = explode(' ', trim(fgets(STDIN)));
-    if (count($arr) < 2 || count($arr) > 3){
-        continue;
+/* Problem 04.	Make a instances
+*  Problem 05.	Add sub class
+*/
+
+use SoftUni\Oop\Car;
+
+/* TEST
+* Nissan X-trail 2007 petrol 5+1 125/n
+* Renault Scenic 2001 diesel 6+1 115/n
+* Audi A6 2001 petrol 5+1 135/n
+* Renault Clio 1995 diesel 4+1 115/n
+*/
+
+
+$arr = readCLI();
+
+$cars = readAndSortCars($arr);
+
+printResult($cars);
+
+function readCLI(): array
+{
+    $arr = [];
+    for ($i = 0; $i < 4; $i++) {
+        $input = explode(' ', trim(fgets(STDIN)));
+
+        if (count($input) != 6) {
+            continue;
+        }
+        $arr[] = $input;
+    }
+    return $arr;
+}
+
+function readAndSortCars(array $arr): array
+{
+    $index = 0;
+    while ($index < count($arr)) {
+
+        $car = new Car(trim($arr[$index][0]), trim($arr[$index][1]),
+            trim($arr[$index][3]), trim($arr[$index][4]), trim($arr[$index][5]));
+
+        $car->setYear(intval(trim($arr[$index][2])));
+
+        $cars[] = $car;
+        $index++;
     }
 
-    $car = new Car(trim($arr[0]), trim($arr[1]));
+    usort($cars, function ($a, $b) {
+        if ($a->getBrand() == $b->getBrand()) {
+            $modelA = $a->getModel();
+            $modelB = $b->getModel();
+            if ($modelA->getModel() == $modelB->getModel()) {
+                if ($a->getYear() > $b->getYear()) {
+                    return 0;
+                }
+                return ($a->getYear() < $b->getYear()) ? -1 : 1;
+            }
+            return ($a->getModel() < $b->getModel()) ? -1 : 1;
+        }
+        return ($a->getBrand() < $b->getBrand()) ? -1 : 1;
 
-    if (count($arr) === 3) {
-        $car->setYear(intval(trim($arr[2])));
-    }
+    });
+    return $cars;
+}
 
-    //TODO SORT
+function printResult(array $cars): void
+{
+    foreach ($cars as $kvp) {
+        $model = $kvp->getModel();
+        echo '*' . $kvp->getBrand() . ',' . $model->getModel() . ',' . $kvp->getYear() . "\n";
+        echo '--' . $model->getEngine() . ',' . $model->getNumberOfSeats() . ',' . $model->getHorsepower() . "\n";
 
-
-    if ($car->getYear() > 0) {
-        echo $car->getModel() . ', ' . $car->getBrand() . ', ' . $car->getYear();
-    } else {
-        echo $car->getModel() . ', ' . $car->getBrand();
     }
 }
