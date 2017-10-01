5f2699a3b8332610b31713c52beb73a2c02ac172
Peter Georgiev <peroxmen@hotmail.com>
2017-10-01 22:41:31 +0300

    Added OOPBasics/Exercises/11.OldestFamilyMember/oldestFamilyMember.php

---
+++ OOPBasics/Exercises/11.OldestFamilyMember/Family.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+class Family
+{
+    private $people = [];
+    private $oldestFamilyMember;
+
+    public function addMember(Person )
+    {
+        
+    }
+}
---
+++ OOPBasics/Exercises/11.OldestFamilyMember/oldestFamilyMember.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+include "Person.php";
+include "Family.php";
+
+$n = intval(trim(fgets(STDIN)));
+for ($i = 0; $i < $n; $i++) {
+
+}
