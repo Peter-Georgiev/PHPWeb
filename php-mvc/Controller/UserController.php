fcdd0e7e91bddfa2b5781cb2afa11092213f3c96
Peter-Georgiev <peter.g.georgiev@gmail.com>
2017-11-13 16:27:57 +0200

    :)

---
+++ php-mvc/Controller/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Controller;
+
+
+class UserController
+{
+
+    public function delete()
+    {
+        echo 'Delete';
+    }
+
+    public function remove()
+    {
+        echo 'Remove';
+    }
+}
