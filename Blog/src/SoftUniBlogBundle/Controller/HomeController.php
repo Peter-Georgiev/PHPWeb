95cc03b260c73d7a0c471fd5f4939b5dc9a51f55
Peter Georgiev <peroxmen@hotmail.com>
2017-11-24 17:57:11 +0200

    :)

--- Blog/src/SoftUniBlogBundle/Controller/HomeController.php
+++ Blog/src/SoftUniBlogBundle/Controller/HomeController.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class HomeController extends Controller
 {
     /**
-     * @Route("/", name="blog_index")
+     * @Route("/", name="blog_indexy")
      * @Method("GET")
      */
     public function indexAction()
